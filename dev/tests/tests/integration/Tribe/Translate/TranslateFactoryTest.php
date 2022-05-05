9675c83378aa38cd727c17a1d9852af2f317d055
Justin Frydman <justin.frydman@gmail.com>
2022-05-05 09:54:22 -0600

    Add translate factory test

---
+++ dev/tests/tests/integration/Tribe/Translate/TranslateFactoryTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Tribe\Weglot\Translate;
+
+use Tribe\Tests\Test_Case;
+use Tribe\Weglot\Translate\Strategies\Html;
+use Tribe\Weglot\Translate\Strategies\Json;
+
+class TranslateFactoryTest extends Test_Case {
+
+	public function test_it_creates_translation_strategy_instances(): void {
+		$factory = $this->container->get( Translation_Factory::class );
+
+		$html = $factory->make( Translate_Subscriber::TYPE_HTML );
+
+		$this->assertInstanceOf( Translatable::class, $html );
+		$this->assertInstanceOf( Html::class, $html );
+
+		$json = $factory->make( Translate_Subscriber::TYPE_JSON );
+
+		$this->assertInstanceOf( Translatable::class, $json );
+		$this->assertInstanceOf( Json::class, $json );
+
+		$xml = $factory->make( Translate_Subscriber::TYPE_XML );
+
+		$this->assertInstanceOf( Translatable::class, $xml );
+		$this->assertInstanceOf( Html::class, $xml );
+
+		// Should use the HTML strategy as the default
+		$fallback = $factory->make( 'Unknown_Class' );
+
+		$this->assertInstanceOf( Translatable::class, $fallback );
+		$this->assertInstanceOf( Html::class, $fallback );
+
+	}
+
+}
